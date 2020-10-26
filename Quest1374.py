d58e9c6c7fb15932af4e7fe4b5c5db97b1ad545c
csengAIDA <christoph.seng.telekom@gmail.com>
2020-10-26 17:01:58 +0100

    second version

--- Quest1374.py
+++ Quest1374.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
+#%%
 #pip install boto3
 
 
-# In[3]:
-
-
+#%%
 import boto3
 
 
-# In[5]:
-
-
+#%%
 s3 = boto3.resource('s3')
 
 
-# In[6]:
-
+#%%
 
 for bucket in s3.buckets.all():
     print(bucket.name)
 
 
-# In[8]:
-
+#%%
 '''
 # Create my second bucket
 response = s3.create_bucket(Bucket='cseng-1374-tuxedo',
@@ -38,29 +30,20 @@ response = s3.create_bucket(Bucket='cseng-1374-tuxedo',
 print(response)
 '''
 
-# In[9]:
 
-
-# check, if new bucket is in the list
+#%% check, if new bucket is in the list
 for bucket in s3.buckets.all():
     print(bucket.name)
 
 
 
-# In[15]:
+#%%
 
-
-# Get the list_buckets response
-
-# # the following did not work, as s3 was the resource and not the client 
-#response = s3.list_buckets()
-
-# so get the client first
+# get the client first
 s3_client = s3.meta.client
 
 
-# In[16]:
-
+#%%
 
 bucket_list = s3_client.list_buckets()
 
@@ -71,7 +54,7 @@ for bucket in bucket_list['Buckets']:
     print(bucket['Name'])
 
 
-# In[19]:
+#%%
 # delete the first bucket created with EC2 instance
 #s3_client.delete_bucket(Bucket='cseng-1374')
 
